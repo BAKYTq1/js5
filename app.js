900325700b730905464a447c744467444d933946
BAKYTq1 <bsainaev@gmail.com>
2024-10-09 18:41:20 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,125 @@
+// const num = parseInt( prompt('Сан жаз'))
+// const num2 = parseInt( prompt('Сан жаз'))
+// const bool = 'Folse'
+// if (num>num2){
+//     alert(`${num}`)
+// } else if (num2>num){
+//       alert(`${num2}`)
+// } else if (num==num2){
+//     alert(`Эко тен`)
+// } else{
+//    alert(`${bool}`)
+// }
+
+// const num = parseInt(prompt('Сан жаз'))
+
+//  if (num>=100){
+//     console.log(`Ото сонун`);
+// } else if(num>=60){
+//         console.log(`Кайра окуу`);
+// }else {
+//     console.log(`Начар`);
+
+// }
+
+// const num = parseInt(prompt('Мезгилдер'))
+
+// switch (num){      
+//     case 1:    
+//     console.log('Кыш');
+//     break;      
+//     case 2:    
+//     console.log('Кыш');
+//     break;      
+//     case 3:    
+//     console.log('Жаз');
+//     break;      
+//     case 4:    
+//     console.log('Жаз');
+//     break;      
+//     case 5:    
+//     console.log('Жаз');
+//     break;      
+//     case 6:    
+//     console.log('Жай');
+//     break;      
+//     case 7:    
+//     console.log('Жай');
+//     break;      
+//     case 8:    
+//     console.log('Жай');
+//     break;      
+//     case 9:    
+//     console.log('Куз');
+//     break;      
+//     case 10:    
+//     console.log('Куз');
+//     break;      
+//     case 11:    
+//     console.log('Куз');
+//     break;    
+//     case 12:    
+//     console.log('Кыш');
+//     break;
+//     default:
+//         console.log('Мындай мезгил жок');
+// }
+//  const num2 = parseInt(prompt('Айлар'))
+
+//  switch(num2){
+//     case 1: 
+//     console.log('Январь');
+//     break;
+//     case 2: 
+//     console.log('Февраль');
+//     break;
+//     case 3: 
+//     console.log('Март');
+//     break;
+//     case 4: 
+//     console.log('Апрель');
+//     break;
+//     case 5: 
+//     console.log('Май');
+//     break;
+//     case 6: 
+//     console.log('Июнь');
+//     break;
+//     case 7: 
+//     console.log('Июль');
+//     break;
+//     case 8: 
+//     console.log('Августь');
+//     break;
+//     case 9: 
+//     console.log('Сентябрь');
+//     break;
+//     case 10: 
+//     console.log('Октябрь');
+//     break;
+//     case 11: 
+//     console.log('Ноябрь');
+//     break;
+//     case 1: 
+//     console.log('Декабрь');
+//     break;
+//     default:
+//         console.log('Мындай ай жок');      
+//  }
+
+// const num3 = parseInt(prompt('Сан жаз'))
+// if (num3 % 2 === 0){
+//     console.log('Жуп сан');
+// } else {
+//     console.log('Так сан');
+// }
+
+// const num = parseInt(prompt('Сан жаз'))
+// const num2 = parseInt(prompt('Дагы сан жаз'))
+// console.log(num+num2);
+// console.log(num-num2);
+// console.log(num*num2);
+// console.log(num/num2);
+
+
+        
